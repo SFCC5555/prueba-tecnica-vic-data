37b407ed792bddb30469cef0552ef227d779be7a
sfcc5555 <sfcc.5555@gmail.com>
2024-01-29 07:38:21 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+function App() {
+  return (
+    <>
+      <h1>Prueba Técnica - VicData </h1>
+      <h5>Severo Fernando Carrasco Cantos</h5>
+      <ul>
+        <li>
+          Calculadora: React + Bootstrap
+          <div>
+            link:{" "}
+            <a href="https://calculator-react-bootstrap.netlify.app/">
+              https://calculator-react-bootstrap.netlify.app/
+            </a>
+          </div>
+          <div>
+            repositiorio:{" "}
+            <a href="https://github.com/SFCC5555/vic-data-react-bootstrap">
+              https://github.com/SFCC5555/vic-data-react-bootstrap
+            </a>
+          </div>
+        </li>
+        <li>
+          Calculadora: React + Tailwind
+          <div>
+            link:{" "}
+            <a href="https://calculator-react-tailwind-sfcc.netlify.app/">
+              https://calculator-react-tailwind-sfcc.netlify.app/
+            </a>
+          </div>
+          <div>
+            repositiorio:{" "}
+            <a href="https://github.com/SFCC5555/vic-data-react-tailwind">
+              https://github.com/SFCC5555/vic-data-react-tailwind
+            </a>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
